cd9b8e7188ca7b41419935db5c5c3d66345b48b8
JavierDelgadoMartinez <javier.delgado@idtconsultores.com>
2021-11-24 22:55:02 -0600

    Cambios de la app terminada

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,50 @@
 
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+const bands = new Bands();
+
+console.log('Init Server');
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Charlie papa'));
+bands.addBand(new Band('Nirvana'));
+bands.addBand(new Band('The clash'));
+
+console.log(bands);
 
 //Mensajes de sockets
 io.on('connection', client => {
   console.log('Cliente se ha conectado');
+
+  client.emit('active-bands', bands.getBands());
+
+
   client.on('event', data => { /* … */ });
   client.on('disconnect', () => {
     console.log('Cliente se ha desconectado');
   });
 
-  client.on('mensaje',(payload) =>{
+  client.on('vote-band',(payload)=>{
+    console.log(payload.id);
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('add-band',(payload)=>{
     console.log(payload);
-    io.emit('mensaje',{admin:'Nuevo mensaje'});
+    bands.addBand(new Band(payload.name));
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('delete-band',(payload)=>{
+    console.log(payload.id);
+    bands.deleteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('emitir-mensaje',(payload)=>{
+    console.log(payload);
+    client.broadcast.emit('nuevo-mensaje',payload);
   });
 });
